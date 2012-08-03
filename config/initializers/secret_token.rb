d6107d95491564d953a0be9423ab064f820130fd
Nick Johannsen <nick@richarddawkins.net>
2012-08-03 16:01:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parallax::Application.config.secret_token = '0d15de3f68226db35c5e46b91ea34652f13ed569fc07cf55a940c169f62dcfeb1bf0cd2266342b7e4b8b3ca8c3143800df001891ba7ce60de2b2dd37fbb58987'
